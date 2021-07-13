048cf5705e0a9f8955520d5f9a86567da598fb53
Nahid-Hassan <nahid.cseru@gmail.com>
2021-07-13 17:33:22 +0600

    view templates test

--- courses/backend/Learn Django by Creating Projects/projects/helloworld/pages/urls.py
+++ courses/backend/Learn Django by Creating Projects/projects/helloworld/pages/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.HomePageView, name='home'),
+    # path('', views.HomePageView, name='home'),
+    path('home/', views.HomePageView.as_view(), name='home'),
+    path('index/', views.IndexPageView.as_view(), name='index'),
 ]
--- courses/backend/Learn Django by Creating Projects/projects/helloworld/pages/views.py
+++ courses/backend/Learn Django by Creating Projects/projects/helloworld/pages/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 
 
-def HomePageView(request):
-    return HttpResponse("Hello, World")
+# def HomePageView(request):
+#     return HttpResponse("Hello, World")
+
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
+
+
+class IndexPageView(TemplateView):
+    template_name = 'pages/index.html'
