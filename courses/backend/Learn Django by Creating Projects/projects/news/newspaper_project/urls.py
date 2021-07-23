66377f6455c29f667dd2975efc8b79b92ec92968
Nahid-Hassan <nahid.cseru@gmail.com>
2021-07-23 19:28:12 +0600

    bootstrap5 in django project

--- courses/backend/Learn Django by Creating Projects/projects/news/newspaper_project/urls.py
+++ courses/backend/Learn Django by Creating Projects/projects/news/newspaper_project/urls.py
@@ -4,7 +4,8 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',TemplateView.as_view(template_name='home.html'), name='home'),
+    # path('',TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', include('pages.urls')),
     path('users/',include('users.urls')),
     path('users/',include('django.contrib.auth.urls')),
 ]
--- courses/backend/Learn Django by Creating Projects/projects/news/users/tests.py
+++ courses/backend/Learn Django by Creating Projects/projects/news/users/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+# from django.test import TestCase
 
-# Create your tests here.
+# # Create your tests here.
+# class SignUpPageTest(TestCase):
+#     username = 'newuser'
+#     email = 'newuser@gmail.com'
+    
+#     def test_signup_page_status_code(self):
+#         response = self.client.get('/users/signup/')
+#         self.assertEqual(response.status_code, 200)
+        
