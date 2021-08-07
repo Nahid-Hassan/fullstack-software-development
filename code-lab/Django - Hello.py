b6ed08cbe2d2f74e4281109e74017d5c161b92f9
Nahid-Hassan <nahid.cseru@gmail.com>
2021-08-07 17:08:32 +0600

    django lightweight helloworld

--- code-lab/Django - Hello.py
+++ code-lab/Django - Hello.py
@@ -6,28 +6,40 @@ $ python 'Django - Hello.py' runserver # 'Django - Hello.py' is the filename
 """
 
 # settings.py
+from django.core.wsgi import get_wsgi_application
 from django.conf.urls import url
 from django.http import HttpResponse
 import sys
+import os
 from django.conf import settings
 
+DEBUG = os.environ.get('DEBUG', 'on') == 'on'
+SECRET_KEY = os.environ.get('SECRET_KEY', '{{ secret_key }}')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+
 settings.configure(
-    # DEBUG=True,
-    ALLOWED_HOSTS = ['*'],
-    # SECRATE_KEY='thisisasecretkey',
+    DEBUG=DEBUG,
+    ALLOWED_HOSTS=ALLOWED_HOSTS,
+    SECRET_KEY=SECRET_KEY,
     ROOT_URLCONF=__name__,
-    # MIDDLEWARE_CLASSES=(
-        # 'django.middleware.common.CommonMiddleware',
-        # 'django.middleware.csrf.CsrfViewMiddleware',
-        # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # ),
+    MIDDLEWARE_CLASSES=(
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ),
 )
 
 # views.py
+
+
+"""
+HTTP Request objects Postmortem:
+
+['COOKIES', 'FILES', 'GET', 'META', 'POST', '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', '_current_scheme_host', '_encoding', '_get_full_path', '_get_post', '_get_raw_host', '_get_scheme', '_initialize_handlers', '_load_post_and_files', '_mark_post_parse_error', '_read_started', '_set_content_type_params', '_set_post', '_stream', '_upload_handlers', 'accepted_types', 'accepts', 'body', 'build_absolute_uri', 'close', 'content_params', 'content_type', 'encoding', 'environ', 'get_full_path', 'get_full_path_info', 'get_host', 'get_port', 'get_raw_uri', 'get_signed_cookie', 'headers', 'is_ajax', 'is_secure', 'method', 'parse_file_upload', 'path', 'path_info', 'read', 'readline', 'readlines', 'resolver_match', 'scheme', 'upload_handlers']
+"""
+
+
 def index(request):
-    print(request)
-    print(dir(request))
-    
     return HttpResponse("<h1>Hello Django.</h1>")
 
 
@@ -35,6 +47,9 @@ def index(request):
 urlpatterns = [
     url(r'^$', index),
 ]
+
+# for wsgi application
+application = get_wsgi_application()
 
 # manage.py
 if __name__ == "__main__":
