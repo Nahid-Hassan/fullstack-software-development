c55cdcbb7d80b6d1bb099715e9c0e9b6b0b82ca5
Nahid-Hassan <nahid.cseru@gmail.com>
2021-08-13 20:55:50 +0600

    add templates in minimal weight django project.

--- courses/backend/light-weight-django/projects/hello-world-multifile-approach/settings.py
+++ courses/backend/light-weight-django/projects/hello-world-multifile-approach/settings.py
@@ -1,10 +1,38 @@
 import os
+from pathlib import Path
 
-ROOT_URLCONF = 'urls' # indicate urls.py
+BASE_DIR = Path(__file__).resolve().parent
+
+
+ROOT_URLCONF = 'urls'  # indicate urls.py
 # DEBUG = True
 # SECRET_KEY = '98234jlds^I*'
 
 # Little Improvement
-ROOT_URLCONF = 'urls' # indicate urls.py
+ROOT_URLCONF = 'urls'  # indicate urls.py
 DEBUG = os.environ.get("DEBUG", "on") == "on"
 SECRET_KEY = os.environ.get("SECRET_KEY", os.urandom(32))
+
+INSTALLED_APPS = [
+    'django.contrib.staticfiles',
+]
+
+
+MIDDLEWARE = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
+
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+    },
+]
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
